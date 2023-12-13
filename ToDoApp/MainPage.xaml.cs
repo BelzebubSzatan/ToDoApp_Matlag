b569277594555d6aae5abdac9c06159c54d259ea
BelzebubSzatan <kacpermatlag@gmail.com>
2023-12-13 10:14:14 +0100

    basic UI

--- ToDoApp/MainPage.xaml.cs
+++ ToDoApp/MainPage.xaml.cs
@@ -4,15 +4,24 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoApp.Model;
 using Xamarin.Forms;
 
 namespace ToDoApp
 {
     public partial class MainPage : ContentPage
     {
+        List<TaskModel> tasks = new List<TaskModel>();
         public MainPage()
         {
             InitializeComponent();
+            tasks.Add(new TaskModel()
+            {
+                ID=Guid.NewGuid(),
+                Title="123",
+                Importance="Ważne",
+            });
+            TasksList.ItemsSource = tasks;
         }
     }
 }
